34dcf6c586f7753b5ab7db1815f12a8127a138fe
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-06 19:14:46 -0600

    06 Bottom Navigation: Listener Select

--- app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
@@ -38,5 +38,26 @@ class MainActivity : AppCompatActivity() {
             .hide(addFragment).commit()
         mFragmentManager.beginTransaction()
             .add(R.id.hostFragment, homeFragment, HomeFragment::class.java.name).commit()
+
+        mBinding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(homeFragment).commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+                R.id.action_add -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(addFragment).commit()
+                    mActiveFragment = addFragment
+                    true
+                }
+                R.id.action_profile -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(profileFragment).commit()
+                    mActiveFragment = profileFragment
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
