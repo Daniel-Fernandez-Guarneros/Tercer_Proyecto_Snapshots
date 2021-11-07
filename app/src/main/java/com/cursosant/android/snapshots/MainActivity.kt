c6edf428b9355415078c836027a9f9906b27c7a9
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-06 18:54:50 -0600

    04 Creación de Fragments

--- app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
@@ -2,10 +2,22 @@ package com.cursosant.android.snapshots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cursosant.android.snapshots.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().add(R.id.hostFragment, HomeFragment()).commit()
     }
 }
