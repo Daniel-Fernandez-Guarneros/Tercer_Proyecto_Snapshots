f7c9b5c38cc36852e22965af3849241311ac4ca0
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-07 23:16:41 -0600

    24 Authentication: Mejoras

--- app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cursosant.android.snapshots
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.cursosant.android.snapshots.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import java.util.*
 
@@ -94,5 +97,18 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            } else {
+                if (IdpResponse.fromResultIntent(data) == null){
+                    finish()
+                }
+            }
+        }
     }
 }
