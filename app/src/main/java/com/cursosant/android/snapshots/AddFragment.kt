935961271c6c8a054cbdcb5498af2ace36c639b3
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-07 22:06:02 -0600

    18 AddFragment: Configuración inicial

--- app/src/main/java/com/cursosant/android/snapshots/AddFragment.kt
+++ app/src/main/java/com/cursosant/android/snapshots/AddFragment.kt
@@ -1,19 +1,42 @@
 package com.cursosant.android.snapshots
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cursosant.android.snapshots.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+
+    private val RC_GELLERY = 18
+
+    private lateinit var mBinding: FragmentAddBinding
+
+    private var mPhotoSelectedUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        mBinding = FragmentAddBinding.inflate(inflater, container, false)
+        return mBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.btnPost.setOnClickListener { postSnapshot() }
+
+        mBinding.btnSelect.setOnClickListener { openGallery() }
+    }
+
+    private fun openGallery() {
+
+    }
+
+    private fun postSnapshot() {
+
+    }
 }
