5c2e6095d8e9c4b41cae8f6b4b7f7b8d4a8c2d46
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-07 23:04:53 -0600

    23 Firebase Authentication

--- app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/snapshots/MainActivity.kt
@@ -5,20 +5,45 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.cursosant.android.snapshots.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 21
 
     private lateinit var mBinding: ActivityMainBinding
 
     private lateinit var mActiveFragment: Fragment
     private lateinit var mFragmentManager: FragmentManager
 
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private var mFirebaseAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        setupAuth()
         setupBottomNav()
+    }
+
+    private fun setupAuth() {
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if (user == null){
+                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(
+                        Arrays.asList(
+                            AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.GoogleBuilder().build())
+                    )
+                    .build(), RC_SIGN_IN)
+            }
+        }
     }
 
     private fun setupBottomNav() {
@@ -59,5 +84,15 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth?.addAuthStateListener(mAuthListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth?.removeAuthStateListener(mAuthListener)
     }
 }
