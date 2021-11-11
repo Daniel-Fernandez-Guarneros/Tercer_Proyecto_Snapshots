b08b1eea6d381cd40dd60311c97d047b22ba533a
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-11-10 19:53:48 -0600

    27 Subir imagen con nombre y ruta dinámico en Storage

--- app/src/main/java/com/cursosant/android/snapshots/AddFragment.kt
+++ app/src/main/java/com/cursosant/android/snapshots/AddFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.cursosant.android.snapshots.databinding.FragmentAddBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -54,7 +55,8 @@ class AddFragment : Fragment() {
     private fun postSnapshot() {
         mBinding.progressBar.visibility = View.VISIBLE
         val key = mDatabaseReference.push().key!!
-        val storageReference = mStorageReference.child(PATH_SNAPSHOT).child("my_photo")
+        val storageReference = mStorageReference.child(PATH_SNAPSHOT)
+            .child(FirebaseAuth.getInstance().currentUser!!.uid).child(key)
         if (mPhotoSelectedUri != null) {
             storageReference.putFile(mPhotoSelectedUri!!)
                 .addOnProgressListener {
